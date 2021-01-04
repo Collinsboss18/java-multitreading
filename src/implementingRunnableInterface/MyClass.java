c038e20dd98a0c7c0cb1e16cd3fefef594d3e381
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-04 14:48:17 +0100

    threads by implementing runnable interface

---
+++ src/implementingRunnableInterface/MyClass.java
@@ -0,0 +1,16 @@
+package implementingRunnableInterface;
+
+class MyClass implements Runnable {
+
+	public void run() {
+		for (int i=0; i<10; i++) System.out.println(Thread.currentThread().getId()+" Value "+i);
+		
+		// To make thread run concurrently
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
