32c2e89631e1aec0cf30b333db297ea4eae21e2a
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-04 14:45:24 +0100

    extending threads

--- src/extentingThread/Demo.java
+++ src/extentingThread/Demo.java
@@ -1,20 +1,5 @@
 package extentingThread;
 
-class MyClass extends Thread {
-
-	public void run() {
-		for (int i=0; i<10; i++) System.out.println(Thread.currentThread().getId()+" Value "+i);
-		
-		// To make thread run concurrently
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-}
-
 public class Demo {
 
 	public static void main(String[] args) {
